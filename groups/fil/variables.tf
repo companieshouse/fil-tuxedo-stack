49ea99ed46bfde1b8c890ab254189c41c22cd545
Marc Ransome <mransome@companieshouse.gov.uk>
2022-02-17 08:47:56 +0000

    Specify log groups for prod services

--- groups/fil/variables.tf
+++ groups/fil/variables.tf
@@ -99,23 +99,7 @@ variable "service_subtype" {
   default     = "fil"
 }
 
-variable "tuxedo_service_logs" {
-  type        = map(list(any))
-  description = "A map whose keys represent server-side tuxedo server groups with lists of objects representing user log files for each server group. Each object is expected to have at a minimum a 'name' key. Optional 'log_retention_in_days' and 'kms_key_id' attributes can be set per-file to override the default values and will apply to both standard error and standard output log groups for that log."
-  default = {
-    # TODO define log files
-    # ef = [
-    # ]
-    # prod = [
-    # ]
-    # scud = [
-    # ]
-    # cabs = [
-    # ]
-  }
-}
-
-variable "tuxedo_user_logs" {
+variable "tuxedo_logs" {
   type        = map(list(any))
   description = "A map whose keys represent server-side tuxedo server groups with lists of objects representing individual log files for each server group. Each object is expected to have at a minimum a 'name' key. A single CloudWatch log group will be created for each object. Optional 'log_retention_in_days' and 'kms_key_id' attributes can be set per-file to override the default values."
   default = {
@@ -124,6 +108,8 @@ variable "tuxedo_user_logs" {
     ]
     prod = [
       { name: "ULOG" }
+      { name: "domain" }
+      { name: "prod" }
     ]
     scud = [
       { name: "ULOG" }
