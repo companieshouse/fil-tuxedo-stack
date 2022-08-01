ebf5570874c305598b298c6093aa6fae46443308
Marc Ransome <mransome@companieshouse.gov.uk>
2022-08-01 11:30:13 +0100

    Add missing conditional attribute

--- groups/fil/kms.tf
+++ groups/fil/kms.tf
@@ -12,6 +12,8 @@ resource "aws_kms_key" "fil" {
 }
 
 resource "aws_kms_alias" "fil" {
+  count = var.ef_presenter_data_bucket_enabled ? 1 : 0
+
   name          = "alias/${local.common_resource_name}"
   target_key_id = aws_kms_key.fil[0].key_id
 }
